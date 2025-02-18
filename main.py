2a9c7d9961b8005c6b42b883e2fd0c873741790d
SalmanAAA <bdod307@gmail.com>
2025-02-18 10:19:16 +0700

    update displayMenu

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ def clear():
 
     else:
         _ = system('clear')
+
+
+def displayMenu():
+    print("\n********Menu**********")
+    print("1. Tambah Jadwal Meeting")
+    print("2. Ubah Jadwal Meeting")
+    print("3. Hapus Jadwal Meeting")
+    print("4. Tampilkan Semua Jadwal Meeting")
+    print("5. Keluar")
+
+    
