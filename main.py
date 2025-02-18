6475343fe911720cb2620c15492db3e2b3da9553
SalmanAAA <bdod307@gmail.com>
2025-02-18 10:38:58 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 def clear():
     if name == 'nt':
         _ = system('cls')
@@ -14,4 +16,42 @@ def displayMenu():
     print("4. Tampilkan Semua Jadwal Meeting")
     print("5. Keluar")
 
-    
+def main():
+    while True :
+        displayMenu()
+        choice = input("Pilih menu: ")
+        if choice == 1:
+            while True:
+                #masukkan cuy tambahkan nama,instansi,waktu,tempat,tentang
+                sub_choice = input("Apakah Anda ingin menambahkan meeting lagi? (y/n): ")
+                if sub_choice.lower() != 'y':
+                    break
+        elif choice == 2:
+            while True:
+                #masukkan cuy ubah tanggal meeting
+                sub_choice = input("Apakah Anda ingin mengubah Jadwal meeting lagi? (y/n): ")
+                if sub_choice.lower() != 'y':
+                    break
+        elif choice == 3:
+            while True:
+                #hapus jadwal meeting
+                sub_choice = input("Apakah Anda ingin menghapus meeting lagi? (y/n): ")
+                if sub_choice.lower() != 'y':
+                    break
+        elif choice == 4:
+            while True:
+                #tampilkan semua jadwal meeting
+                sub_choice = input("Apakah Anda ingin kembali ke menu? (y/n): ")
+                if sub_choice.lower() != 'y':
+                    break
+        elif choice == 5:
+            while True:
+                print("EXIT")
+                break
+
+
+
+
+
+if __name__ == '__main__':
+    main()
